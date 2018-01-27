3e06e269dfaeb695dd6b44897a6c6e123a31e1e6
Piero Dalle Pezze <piero.dallepezze@gmail.com>
2018-01-27 13:45:58 +0000

    create a script for removing CRAN non-standard files/directories found at top level. This script will routinely be executed by Travis-CI.

---
+++ run_for_cran.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+#
+# This file is part of sbpiper.
+#
+# sbpiper is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sbpiper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with sbpiper.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#
+#
+# This script removes CRAN non-standard files/directories found at top level.
+# It should be executed before the following commands:
+# 
+# R CMD build .
+# R CMD check *tar.gz --as-cran
+
+
+# remove files for github
+rm CODE_OF_CONDUCT.md
+rm CONTRIBUTING.md
+rm ISSUE_TEMPLATE.md
+rm .travis.yml
+
+# remove files for conda
+rm -rf conda_recipe
+rm environment.yaml
+
+# remove this script
+rm run_for_cran.sh
